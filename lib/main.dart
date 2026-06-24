229d0783078823501d31b108380a49880a90f065
HuyLe002 <huylndse182703@fpt.edu.vn>
2026-06-24 07:23:51 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'controllers/publication_controller.dart';
+import 'controllers/analysis_controller.dart';
+import 'controllers/user_controller.dart';
+import 'screens/home_screen.dart';
+import 'utils/app_theme.dart';
+
+void main() {
+  runApp(const JournalTrendAnalyzerApp());
+}
+
+/// Lớp gốc của ứng dụng, thiết lập quản lý trạng thái và giao diện chính.
+/// Đã gỡ bỏ khung điện thoại ép buộc trên Web để tối ưu cho màn hình lớn.
+class JournalTrendAnalyzerApp extends StatelessWidget {
+  const JournalTrendAnalyzerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PublicationController()),
+        ChangeNotifierProvider(create: (_) => AnalysisController()),
+        ChangeNotifierProvider(create: (_) => UserController()),
+      ],
+      child: MaterialApp(
+        title: 'Journal Trend Analyzer',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
